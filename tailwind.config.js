bac6b9b8220de01478481b717216714af7b3533e
rinkigond5678 <rinkigond5678@gmail.com>
2024-09-16 21:57:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#ee081',
+
+      },
+      fontFamily: {
+        'display':['cinzel','sans-serif'],
+        'body':['merriweather','sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
